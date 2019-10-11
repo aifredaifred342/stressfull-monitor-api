aa8c2d8a47e67854caa566ebe6668e4f9ba90dab
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-10-11 18:52:39 +0200

    init project

---
+++ src/main/java/com/intelligent/systems/aifred/AifredApplication.java
@@ -0,0 +1,13 @@
+package com.intelligent.systems.aifred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AifredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AifredApplication.class, args);
+	}
+
+}
